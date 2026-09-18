64bbfa9ce90e20bb687aba471c4bd7e041259176
AL3XZIG <89033213344@mail.ru>
2026-09-18 16:30:41 +0400

    fix: remove committed CMake build artifacts

--- build/common/CMakeFiles/isb-common.dir/src/capability_state.cpp.o.d
+++
@@ -1,3 +0,0 @@
-common/CMakeFiles/isb-common.dir/src/capability_state.cpp.o: \
- /workspace/common/src/capability_state.cpp /usr/include/stdc-predef.h \
- /workspace/common/include/isb/common/capability_state.hpp
--- build/providers/cuda/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/providers/cuda
-# Build directory: /workspace/build/providers/cuda
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(isb-cuda-tests "/workspace/build/providers/cuda/isb-cuda-tests")
-set_tests_properties(isb-cuda-tests PROPERTIES  _BACKTRACE_TRIPLES "/workspace/providers/cuda/CMakeLists.txt;25;add_test;/workspace/providers/cuda/CMakeLists.txt;0;")
